ed2fa158de4b2282a0e0046fc533f88976314529
jansdhillon <77344313+jansdhillon@users.noreply.github.com>
2023-04-20 17:07:15 -0700

    fixed deployment and title and icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
-import Image from 'next/image';
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
+import Head from "next/head";
 
 export default function App() {
 
   return (
-    <div className="bg-gradient-to-br from-purple-600 to-indigo-900 text-white min-h-screen font-roboto">
+    <main className="bg-gradient-to-br from-purple-600 to-indigo-900 text-white min-h-screen font-roboto">
+      <Head>
+      <title>Salmon Arm Painting</title>
+      <link rel="icon" href="/saplogov3.png" />
+      </Head>
       <NavBar logo={"/saplogov3.png"} />
       <Home logo={"saplogov3.png"}/>
       <About/>
       <Contact />
       <Footer />
-    </div>
-  )
+    </main>
+  );
 }
